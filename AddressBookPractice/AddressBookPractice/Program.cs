e8d55ad2a477879f9a9b4de9cd04962ea05b6275
tutorial6 <manapure.vaibhav542@gmail.com>
2022-04-03 13:02:28 +0530

    initial commit

---
+++ AddressBookPractice/AddressBookPractice/Program.cs
@@ -0,0 +1,21 @@
+﻿using AddressBookPractice;
+using System;
+
+namespace Address_Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("-----------------------");
+
+            ContactOperation contactOperation = new ContactOperation();
+            contactOperation.addContact("victor", "singha", "addrezzz", "cityx", "statez", "766452", "9764510067", "manapure.vaibhav542@gmail.com");
+            contactOperation.showList();
+
+            Console.ReadKey();
+        }
+    }
+}
