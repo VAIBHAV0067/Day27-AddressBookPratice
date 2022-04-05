39d1a9c2d0dc698eac9b77b098646d6c4ff412c6
tutorial6 <manapure.vaibhav542@gmail.com>
2022-04-05 18:36:31 +0530

    New Contact

--- AddressBookPractice/AddressBookPractice/Program.cs
+++ AddressBookPractice/AddressBookPractice/Program.cs
@@ -10,9 +10,27 @@ namespace Address_Book
             Console.WriteLine("Welcome To Address Book");
             Console.WriteLine("-----------------------");
             Console.WriteLine("-----------------------");
+            Console.WriteLine("--Add Contact Details--");
+
+            Console.WriteLine("First Name:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Last Name:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Address:");
+            string address = Console.ReadLine();
+            Console.WriteLine("City:");
+            string city = Console.ReadLine();
+            Console.WriteLine("State:");
+            string state = Console.ReadLine();
+            Console.WriteLine("Zip Code:");
+            string zip = Console.ReadLine();
+            Console.WriteLine("Phone Number:");
+            string phoneNumber = Console.ReadLine();
+            Console.WriteLine("Email Address:");
+            string email = Console.ReadLine();
 
             ContactOperation contactOperation = new ContactOperation();
-            contactOperation.addContact("victor", "singha", "addrezzz", "cityx", "statez", "766452", "9764510067", "manapure.vaibhav542@gmail.com");
+            contactOperation.addContact("Vaibhav", "Manapure", "addrezzz", "cityx", "Maharshtra", "766452", "9764510067", "manapure.vaibhav542@gmail.com");
             contactOperation.showList();
 
             Console.ReadKey();
